3a4be3423a34a5c11c31fa20287edb50a3d4ffdd
Abdikarim-dev <cabdikariim405@gmail.com>
2025-10-15 20:04:52 +0300

    created auth section successfully

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const dotenv = require("dotenv")
+const cors = require("cors")
 const chalk = require("chalk")
 
 const { connectDB } = require("./config/config")
@@ -7,13 +8,16 @@ const app = express()
 
 dotenv.config()
 
+const authRouter = require("./routes/Auth")
 const userRouter = require("./routes/User")
 
 const PORT = process.env.PORT || 8007
 
 app.use(express.json());
+app.use(cors())
 connectDB()
 
+app.use("/api/auth", authRouter)
 app.use("/api/user", userRouter)
 
 app.listen(PORT, () => {
